35aed81869fdbd34df6c37436d73f62c2a9aca80
Grzegorz <ystro21@wp.pl>
2023-02-27 14:12:31 +0100

    city search component

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cfa540e13amsh82d863a8d2201c5p10f42bjsnae5b22be0394',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
