a1cf408aca0c5ed4c4702ae36862401cb112d724
Grzegorz <ystro21@wp.pl>
2023-02-28 13:04:04 +0100

    Data from weather dynamic

--- src/api.js
+++ src/api.js
@@ -7,3 +7,7 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "1ad2a4f9f38dfb262086fd95f9fbf7bf"
