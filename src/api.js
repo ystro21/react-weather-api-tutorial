c19f95c195399730d63d0993b5d914dbb8fabf86
Grzegorz <ystro21@wp.pl>
2023-03-01 11:49:17 +0100

    Gitignore apiKey

--- src/api.js
+++ src/api.js
@@ -1,13 +1,3 @@
-export const geoApiOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'cfa540e13amsh82d863a8d2201c5p10f42bjsnae5b22be0394',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
-};
-
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
-
-export const WEATHER_API_KEY = "1ad2a4f9f38dfb262086fd95f9fbf7bf"
